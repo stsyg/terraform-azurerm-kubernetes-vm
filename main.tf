c395230f0884b90a55e9e3bf5b2b27512a6608f5
Sergiy <23329191+stsyg@users.noreply.github.com>
2022-09-30 22:07:38 -0600

    minor changes #2

--- main.tf
+++ main.tf
@@ -401,14 +401,14 @@ resource "azurerm_lb_probe" "lbp01" {
 
 # Network Load Balancer - load balancing rule with health probe
 resource "azurerm_lb_rule" "lbr01" {
-  name                           = "${var.prefix}-${var.environment}-lbr01"
-  resource_group_name            = azurerm_resource_group.rg01.name
+  name = "${var.prefix}-${var.environment}-lbr01"
+  #  resource_group_name            = azurerm_resource_group.rg01.name
   loadbalancer_id                = azurerm_lb.lb01.id
   frontend_ip_configuration_name = "${var.prefix}-${var.environment}-apiserver"
   protocol                       = "Tcp"
   frontend_port                  = "6443"
   backend_port                   = "6443"
-  backend_address_pool_id        = azurerm_lb_backend_address_pool.bap01.id
+  #  backend_address_pool_id        = azurerm_lb_backend_address_pool.bap01.id
 
   # attach health probe
   probe_id = azurerm_lb_probe.lbp01.id
